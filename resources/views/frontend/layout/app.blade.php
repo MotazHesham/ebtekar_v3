522d61dd6eb288c1bbcc1ad7aec7ff8ae2afe6d6
Motaz Hesham <motazhesham01@gmail.com>
2024-03-31 15:50:24 +0200

    google tag manager

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -77,12 +77,12 @@
 
     @if(app()->isProduction() && $site_settings->id == 1) 
         <!-- Google Tag Manager -->
-            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KNZ4MQG');</script>
-        <!-- End Google Tag Manager -->
+            })(window,document,'script','dataLayer','GTM-PM74GFD2');</script>
+        <!-- End Google Tag Manager -->
         
         <!-- Facebook Pixel Code -->
         <script>
@@ -119,9 +119,9 @@
     
     @if(app()->isProduction() && $site_settings->id == 1) 
         <!-- Google Tag Manager (noscript) -->
-            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KNZ4MQG"
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PM74GFD2"
             height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
+        <!-- End Google Tag Manager (noscript) -->
     @endif
 
     <!-- loader start -->
