93e0fa3951540d428eea43c32ad59a8ae4841c99
Motaz Hesham <motazhesham01@gmail.com>
2024-11-28 15:18:25 +0200

    repoort social product

--- resources/views/admin/receiptSocials/partials/products-report.blade.php
+++ resources/views/admin/receiptSocials/partials/products-report.blade.php
@@ -1,21 +1,61 @@
 <h5>تقارير المنتجات المطلوبة </h5>
-<table class="table">
+<table class="table datatable-reportt datatable">
     <thead>
         <tr>
             <th scope="col">#</th>
+            <th scope="col">id</th>
             <th scope="col">المنتج</th>
             <th scope="col">الكمية المطلوبة</th> 
             <th scope="col">الاجمالي</th> 
         </tr>
     </thead>
     <tbody> 
+        @php
+            $total = 0;
+            $quantity = 0;
+        @endphp
         @foreach ($products as $key => $product)
+            @php
+                $total += $product->total_cost;
+                $quantity += $product->quantity;
+            @endphp
             <tr>
-                <th scope="row">{{ $key + 1 }}</th>
+                <th scope="row"></th>
+                <td>{{ $key + 1 }}</td>
                 <td>{{ $product->title }}</td>
                 <td>{{ $product->quantity}}</td> 
                 <td>{{ $product->total_cost}}</td> 
             </tr> 
         @endforeach 
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>الاجمالي: {{ $quantity }} </td>
+            <td>الاجمالي: {{ $total }} </td>
+        </tr>
     </tbody>
 </table>
+
+
+<script>
+    $(function() {
+        let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons) 
+
+        $.extend(true, $.fn.dataTable.defaults, {
+            orderCellsTop: true,
+            order: [
+                [1, 'desc']
+            ],
+            pageLength: 25,
+        });
+        let table = $('.datatable-reportt:not(.ajaxTable)').DataTable({
+            buttons: dtButtons
+        })
+        $('a[data-toggle="tab"]').on('shown.bs.tab click', function(e) {
+            $($.fn.dataTable.tables(true)).DataTable()
+                .columns.adjust();
+        });
+
+    })
+</script>
