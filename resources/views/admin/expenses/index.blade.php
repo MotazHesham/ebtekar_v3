0fa9797924c7623a555ae12cb08c2c75710598f5
Motaz Hesham <motazhesham01@gmail.com>
2024-12-23 22:05:43 +0200

    fix paginate expanes and incomes

--- resources/views/admin/expenses/index.blade.php
+++ resources/views/admin/expenses/index.blade.php
@@ -101,27 +101,4 @@
             {{ $expenses->link() }}
         </div>
     </div>
-@endsection
-@section('scripts')
-    @parent
-    <script>
-        $(function() {
-            let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons) 
-            $.extend(true, $.fn.dataTable.defaults, {
-                orderCellsTop: true,
-                order: [
-                    [1, 'desc']
-                ],
-                pageLength: 100,
-            });
-            let table = $('.datatable-Expense:not(.ajaxTable)').DataTable({
-                buttons: dtButtons
-            })
-            $('a[data-toggle="tab"]').on('shown.bs.tab click', function(e) {
-                $($.fn.dataTable.tables(true)).DataTable()
-                    .columns.adjust();
-            });
-
-        })
-    </script>
-@endsection
+@endsection 
--- resources/views/admin/incomes/index.blade.php
+++ resources/views/admin/incomes/index.blade.php
@@ -99,28 +99,4 @@
             {{ $incomes->link() }}
         </div>
     </div>
-@endsection
-@section('scripts')
-    @parent
-    <script>
-        $(function() {
-            let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons) 
-
-            $.extend(true, $.fn.dataTable.defaults, {
-                orderCellsTop: true,
-                order: [
-                    [1, 'desc']
-                ],
-                pageLength: 100,
-            });
-            let table = $('.datatable-Income:not(.ajaxTable)').DataTable({
-                buttons: dtButtons
-            })
-            $('a[data-toggle="tab"]').on('shown.bs.tab click', function(e) {
-                $($.fn.dataTable.tables(true)).DataTable()
-                    .columns.adjust();
-            });
-
-        })
-    </script>
-@endsection
+@endsection 
